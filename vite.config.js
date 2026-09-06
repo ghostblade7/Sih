e906af6a2061c14bf3405eb35b11bad982ab010c
ghostblade7 <rockysencr7@gmail.com>
2026-09-06 14:46:04 +0530

    Include profile page in Vite build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(process.cwd(), 'index.html'),
         login: resolve(process.cwd(), 'login.html'),
+        profile: resolve(process.cwd(), 'profile.html'),
         auth: resolve(process.cwd(), 'auth.html'),
         auth2: resolve(process.cwd(), 'auth2.html'),
         auth3: resolve(process.cwd(), 'auth3.html')
