0c8d6505bd91e21b6ad6088f6f213792501b11b9
ghostblade7 <rockysencr7@gmail.com>
2026-09-06 23:11:31 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "YOUR_SENDER_ID",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "YOUR_APP_ID"
+  apiKey: "AIzaSyCeuizWkJrvp2_iOxD4UOFL12Os2kOKybQ",
+  authDomain: "living-india-7f73b.firebaseapp.com",
+  projectId: "living-india-7f73b",
+  storageBucket: "living-india-7f73b.firebasestorage.app",
+  messagingSenderId: "439601825953",
+  appId: "1:439601825953:web:cc38c29d819b86de03c8e0",
+  measurementId: "G-N76WYJXS7K"
 };
 
 const app = initializeApp(firebaseConfig);
