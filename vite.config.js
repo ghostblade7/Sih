4ab4b71696eab51281a51120e6ec0b7a6e1e4621
ghostblade7 <rockysencr7@gmail.com>
2026-09-06 21:30:09 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import {defineConfig} from 'vite'
-import {resolve} from 'node:path'
-
-export default defineConfig({
-  base:'/',
-  build:{rollupOptions:{input:{main:resolve(process.cwd(),'index.html'),auth:resolve(process.cwd(),'auth.html'),profile:resolve(process.cwd(),'profile.html')}}}
-})
